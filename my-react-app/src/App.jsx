a854c93394c2c3366e17e28b4cc320453ba2c268
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-26 12:52:50 +0300

    Made some simple card componenets

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Food from './Food.jsx';
+import Card from './Card.jsx';
 function App() {
 
   return(
@@ -8,7 +9,9 @@ function App() {
     <Header></Header>
     <Food></Food>
     <Footer></Footer>
-    
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
 
     </>
   );
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="Profile Picture"></img>
+            <h2 className="card-title">Tendo Calvin</h2>
+            <p className="card-text">I am a Software Enginner and I learn new technologies in my free time</p>
+        </div>
+    );
+}
+
+export default Card
